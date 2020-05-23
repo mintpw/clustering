84bfc90b68c923037ff04d2305e52db0d1599bc0
mintpw <mintpw@outlook.com>
2020-05-23 20:14:21 +0300

    first commit

---
+++ precache-manifest.116ed52c9973a91e17912fa0413854c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748083a4606f612e4bd5eb24eb0eaa00",
+    "url": "/clustering/index.html"
+  },
+  {
+    "revision": "12e3a07961e14e2e375d",
+    "url": "/clustering/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fb5b81be8d232bb84619",
+    "url": "/clustering/static/css/main.7bb59e8d.chunk.css"
+  },
+  {
+    "revision": "12e3a07961e14e2e375d",
+    "url": "/clustering/static/js/2.edf509ab.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/clustering/static/js/2.edf509ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5b81be8d232bb84619",
+    "url": "/clustering/static/js/main.cd919b35.chunk.js"
+  },
+  {
+    "revision": "44e495f3c4179f4a21b8",
+    "url": "/clustering/static/js/runtime-main.6bbbf9da.js"
+  }
+]);
